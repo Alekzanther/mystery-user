472725f4a2d943fd13592a5a9da5e5f7065bb461
Alexander Lindberg <alexanderlndbrg@gmail.com>
2020-11-01 13:18:38 +0100

    Refactor and polish main app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders fetch button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Fetch/i);
+  const linkElement = screen.getByText(/Start/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
-import { Container, Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import FetchUserButtonIcon from "@material-ui/icons/Forward";
 import FetchUser from "./api/FetchUser";
 import UserCard from "./containers/UserCard";
 import LoadingIndicator from "./components/LoadingIndicator";
 import User from "./types/User";
+import ErrorIndicator from "./components/ErrorIndicator";
 import "./App.css";
 
+const useStyles = makeStyles((theme) => ({
+  fetchButton: {
+    margin: theme.spacing(15),
+    padding: theme.spacing(2, 3, 2, 4),
+  },
+}));
 export default function App() {
+  const classes = useStyles();
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [currentError, setCurrentError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [buttonText, setButtonText] = useState<string>("Fetch your first user");
+  const [buttonText, setButtonText] = useState<string>("Start exploring!");
 
   function fetchUserClick() {
     setLoading(true);
     setCurrentUser(null);
+
     FetchUser()
       .then((user) => {
         setLoading(false);
@@ -23,32 +34,24 @@ export default function App() {
       })
       .catch((reason) => {
         console.log(reason);
-        setCurrentError("Something went wrong");
+        setCurrentError("Something went wrong!");
         setButtonText("Try again");
+        setLoading(false);
       });
-  }
-
-  if (currentError != null) {
-    return (
-      <div className="App-content">
-        {currentError}
-        <Button variant="contained" onClick={() => fetchUserClick()}>
-          Try again!
-        </Button>
-      </div>
-    );
   }
 
   return (
     <div className="App-content">
+      {currentError && <ErrorIndicator errorText={currentError.toString()} />}
       {loading && <LoadingIndicator />}
-      {currentError && <div>{currentError}</div>}
       {currentUser && <UserCard {...currentUser}></UserCard>}
       {!loading && (
         <Button
+          className={classes.fetchButton}
           id="fetchButton"
           variant="contained"
           color="primary"
+          endIcon={<FetchUserButtonIcon />}
           onClick={() => fetchUserClick()}>
           {buttonText}
         </Button>
